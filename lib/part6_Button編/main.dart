347c3758a6d601ebe9f3a2a8c297d2af18d22545
Shiraishi-Shodai <siranosuke1227@gmail.com>
2023-10-08 21:52:16 +0900

    ボタンを学習

--- lib/part6_Button編/main.dart
+++ lib/part6_Button編/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const String? a = null;
-  const text = Text("$a" ?? 'null');
+  
+      xxx() {
+      debugPrint("通信を始めます"); //デバッグコンソールに文字列を出力
+      debugPrint("通信中です");
+      debugPrint("通信が終りました");
+    }
 
-  const app = MaterialApp(
+    final button = ElevatedButton(
+      onPressed: xxx, //nullを設定するとボタンが押せなくなる
+      ///ボタンを押したときに実行する関数を指定
+      child: Text("押してみて"),
+
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.green
+      )
+    );
+
+  final app = MaterialApp(
     home: Scaffold(
-      body: Center(child: text),
+      body: Center(child: button)
     ),
   );
 
